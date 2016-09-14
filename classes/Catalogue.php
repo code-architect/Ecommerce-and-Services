ec406d0625f9052c442b8c82996e603d845a3025
code-architect <codearchitectdeveloper@gmail.com>
2016-09-14 20:44:02 +0530

    Class Catalogue : getCategory, getProducts
    Added a new page displaying images of that category by id

--- classes/Catalogue.php
+++ classes/Catalogue.php
@@ -3,8 +3,13 @@
 class Catalogue extends Application {
 
     private $_table = 'categories';
+    private $_table_2 = 'products';
 
 
+    /**
+     * Get all the categories
+     * @return array
+     */
     public function getCategories()
     {
         $sql = "SELECT * FROM `{$this->_table}` ORDER BY `name` ASC";
@@ -12,8 +17,34 @@ class Catalogue extends Application {
         return $this->db->fetchAll($sql);
     }
 
+//---------------------------------------------------------------------------------//
 
 
+    /**
+     * Get the Category by id
+     * @param $id
+     * @return mixed
+     */
+    public function getCategory($id)
+    {
+        $sql = "SELECT * FROM `{$this->_table}` WHERE `id` = '".$this->db->escape($id)."'";
+        return $this->db->fetchOne($sql);
+    }
 
+//---------------------------------------------------------------------------------//
+
+
+    /**
+     * Getting products by category id
+     * @param $cat
+     */
+    public function getProducts($cat)
+    {
+        $sql = "SELECT * FROM `{$this->_table_2}` WHERE `category` = '".$this->db->escape($cat)."' ORDER BY `date` desc";
+        return $this->db->fetchAll($sql);
+    }
+
+//---------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------//
 //---------------------------------------------------------------------------------//
 }
---
+++ pages/catalogue.php
@@ -0,0 +1,58 @@
+<?php
+//check if the category parameter exists or not
+$cat = Url::getParam('category');
+
+if(empty($cat))
+{
+    require_once("error.php");
+} else {
+
+    $objCatalogue = new Catalogue();
+    $category = $objCatalogue->getCategory($cat);
+
+    // check if there is a category exists or not
+    if(empty($category))
+    {
+        require_once("error.php");
+    }else{
+
+        $rows = $objCatalogue->getProducts($cat);
+
+        require_once('_header.php');
+?>
+
+    <h1>Catalogue :: <?php echo $category['name']; ?></h1>
+
+    <?php
+        // if data exists then do this
+        if(!empty($rows))
+        {
+            foreach($rows as $row)
+            {
+                ?>
+                <div class="catalogue_wrapper">
+                    <div class="catalogue_wrapper_left">
+                        <?php
+                        $image =!empty($row['image']) ?
+                            'media/catalogue/'.$row['image'] :
+                            'media/catalogue/unavailable.png';
+
+                        $width = Helper::getImgSize($image, 0);
+                        $width = $width > 120 ? 120 : $width;
+                        ?>
+
+                        <a href="/?page="></a>
+                    </div>
+                </div>
+                <?php
+            }
+        }
+    ?>
+
+<?php
+        require_once('_footer.php');
+    }
+}
+
+?>
+
